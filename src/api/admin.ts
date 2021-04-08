ff88ad47453eb38ef69a4f21ebfb35a08e04814e
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2021-04-08 17:41:01 +0200

    Refactored api

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,21 +1,46 @@
 const ADMIN_API_ROOT = '/api/admin'
 
-export function list_users(): Promise<string[]> {
+type RequestOptions = { method?: string; type?: XMLHttpRequestResponseType }
+
+function sendPromise(
+    service: string,
+    data: string | undefined,
+    options: RequestOptions = {}
+): Promise<XMLHttpRequest> {
+    let default_options = {
+        method: 'POST',
+        type: 'json',
+    }
+    let filled_options = Object.assign(default_options, options)
     return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            resolve(postreq.response)
+        let loginreq = new XMLHttpRequest()
+        loginreq.onload = ev => {
+            if (loginreq.status === 401) {
+                reject(Error('Unauthorized'))
+            }
+            resolve(loginreq)
         }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('GET', `${ADMIN_API_ROOT}/users`, true)
-        postreq.send()
+        loginreq.onerror = reject
+        loginreq.onabort = reject
+        loginreq.responseType = filled_options.type
+        loginreq.open(
+            filled_options.method,
+            `${ADMIN_API_ROOT}/${service}`,
+            true
+        )
+        loginreq.send(data)
     })
+}
+function requestPromise(
+    service: string,
+    options: RequestOptions = {}
+): Promise<XMLHttpRequest> {
+    return sendPromise(service, undefined, options)
+}
+
+export async function list_users(): Promise<string[]> {
+    const x = await requestPromise('users', { method: 'GET' })
+    return x.response
 }
 
 interface GroupSummaryRaw {
@@ -29,176 +54,97 @@ export interface GroupSummary {
     users: string[]
 }
 
-export function list_groups(): Promise<GroupSummary[]> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            if (postreq.status !== 200) {
-                reject()
-                return
-            }
-            let group_raw: GroupSummaryRaw[] = postreq.response
-            let group = group_raw.map(v => {
-                let users: string[]
-                if (v.users === null) {
+export async function list_groups(): Promise<GroupSummary[]> {
+    const x = await requestPromise('groups', { method: 'GET' })
+    if (x.status !== 200) {
+        throw undefined
+    }
+    let group_raw: GroupSummaryRaw[] = x.response
+    let group = group_raw.map(v => {
+        let users: string[]
+        if (v.users === null) {
+            users = []
+        } else {
+            try {
+                let temp_users = JSON.parse(v.users) as string[] | [null]
+                if (temp_users[0] === null) {
+                    console.assert(temp_users.length == 1)
                     users = []
                 } else {
-                    try {
-                        let temp_users = JSON.parse(v.users) as
-                            | string[]
-                            | [null]
-                        if (temp_users[0] === null) {
-                            console.assert(temp_users.length == 1)
-                            users = []
-                        } else {
-                            users = temp_users as string[]
-                        }
-                    } catch (error) {
-                        users = [v.users]
-                    }
+                    users = temp_users as string[]
                 }
-                let result: GroupSummary = {
-                    groupid: v.groupid,
-                    groupname: v.groupname,
-                    users,
-                }
-                return result
-            })
-            resolve(group)
-        }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('GET', `${ADMIN_API_ROOT}/groups`, true)
-        postreq.send()
-    })
-}
-
-export function add_group(groupname: string): Promise<number> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            if (postreq.status == 200) {
-                resolve(parseInt(postreq.response))
-            } else {
-                reject()
+            } catch (error) {
+                users = [v.users]
             }
         }
-        postreq.onerror = () => {
-            reject()
+        let result: GroupSummary = {
+            groupid: v.groupid,
+            groupname: v.groupname,
+            users,
         }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('PUT', `${ADMIN_API_ROOT}/group`, true)
-        postreq.send(JSON.stringify(groupname))
+        return result
     })
+    return group
 }
 
-export function remove_group(groupname: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.onload = () => {
-            if (postreq.status == 200) {
-                resolve()
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('DELETE', `${ADMIN_API_ROOT}/group`, true)
-        postreq.send(JSON.stringify(groupname))
-    })
+export async function add_group(groupname: string): Promise<number> {
+    const x = await sendPromise('group', groupname, { method: 'PUT' })
+    if (x.status !== 200) {
+        throw undefined
+    }
+    return x.response
 }
 
-export function add_group_user(
+export async function remove_group(groupname: string): Promise<void> {
+    const x = await sendPromise('group', groupname, { method: 'DELETE' })
+    if (x.status !== 200) {
+        throw undefined
+    }
+}
+
+export async function add_group_user(
     groupname: string,
     username: string
 ): Promise<void> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.onload = () => {
-            if (postreq.status == 200) {
-                resolve()
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('PUT', `${ADMIN_API_ROOT}/group_user`, true)
-        postreq.send(JSON.stringify({ username, groupname }))
-    })
+    const x = await sendPromise(
+        'group_user',
+        JSON.stringify({ username, groupname }),
+        { method: 'PUT' }
+    )
+    if (x.status !== 200) {
+        throw undefined
+    }
 }
 
-export function remove_group_user(
+export async function remove_group_user(
     groupname: string,
     username: string
 ): Promise<void> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.onload = () => {
-            if (postreq.status == 200) {
-                resolve()
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('DELETE', `${ADMIN_API_ROOT}/group_user`, true)
-        postreq.send(JSON.stringify({ username, groupname }))
+    const x = await sendPromise(
+        'group_user',
+        JSON.stringify({ username, groupname }),
+        { method: 'DELETE' }
+    )
+    if (x.status !== 200) {
+        throw undefined
+    }
+}
+
+export async function remove_user(username: string): Promise<void> {
+    const x = await sendPromise('login', JSON.stringify({ username }), {
+        method: 'DELETE',
     })
 }
 
-export function remove_user(username: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.onload = () => {
-            resolve()
+export async function add_user(
+    username: string,
+    password: string
+): Promise<void> {
+    const x = await sendPromise(
+        'login',
+        JSON.stringify({ username, password }),
+        {
+            method: 'PUT',
         }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('DELETE', `${ADMIN_API_ROOT}/login`, true)
-        postreq.send(JSON.stringify({ username }))
-    })
-}
-
-export function add_user(username: string, password: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.onload = () => {
-            resolve()
-        }
-        postreq.onerror = () => {
-            reject()
-        }
-        postreq.onabort = () => {
-            reject()
-        }
-        postreq.open('PUT', `${ADMIN_API_ROOT}/login`, true)
-        postreq.send(JSON.stringify({ username, password }))
-    })
+    )
 }
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,115 +1,80 @@
 const API_ROOT = '/api'
 
-export function logout(): Promise<void> {
+type RequestOptions = { method?: string; type?: XMLHttpRequestResponseType }
+
+function sendPromise(
+    service: string,
+    data: any,
+    options: RequestOptions = {}
+): Promise<XMLHttpRequest> {
+    let default_options = {
+        method: 'POST',
+        type: 'json',
+    }
+    let filled_options = Object.assign(default_options, options)
     return new Promise((resolve, reject) => {
         let loginreq = new XMLHttpRequest()
         loginreq.onload = ev => {
-            switch (loginreq.status) {
-                case 200:
-                    resolve()
-                    return
-                default:
-                    console.warn(
-                        'Unknown status',
-                        loginreq.status,
-                        loginreq.response
-                    )
-                    reject()
+            if (loginreq.status === 401) {
+                reject(Error('Unauthorized'))
             }
+            resolve(loginreq)
         }
-        loginreq.onerror = ev => {
-            console.warn('Error logout', ev)
-            reject()
-        }
-        loginreq.onabort = ev => {
-            console.warn('Abort logout', ev)
-            reject()
-        }
-        loginreq.open('POST', `${API_ROOT}/logout`, true)
-        loginreq.send()
+        loginreq.onerror = reject
+        loginreq.onabort = reject
+        loginreq.responseType = filled_options.type
+        loginreq.open(filled_options.method, `${API_ROOT}/${service}`, true)
+        loginreq.send(data)
     })
 }
+function requestPromise(
+    service: string,
+    options: RequestOptions = {}
+): Promise<XMLHttpRequest> {
+    return sendPromise(service, undefined, options)
+}
 
-export function check_login(
+export async function logout(): Promise<void> {
+    const x = await requestPromise('logout')
+    switch (x.status) {
+        case 200:
+            return
+        default:
+            console.warn('Unknown status', x.status, x.response)
+            throw undefined
+    }
+}
+
+export async function check_login(
     username: string,
     password: string
 ): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        let loginreq = new XMLHttpRequest()
-        loginreq.responseType = 'json'
-        loginreq.onload = ev => {
-            switch (loginreq.status) {
-                case 200:
-                    console.assert(typeof loginreq.response == 'boolean')
-                    resolve(loginreq.response)
-                    return
-                default:
-                    console.warn(
-                        'Unknown status',
-                        loginreq.status,
-                        loginreq.response
-                    )
-                    reject()
-            }
-        }
-        loginreq.onerror = ev => {
-            console.warn('Error login', ev)
-            reject()
-        }
-        loginreq.onabort = ev => {
-            console.warn('Abort login', ev)
-            reject()
-        }
-        let json_data = JSON.stringify({
-            username: username,
-            password: password,
-        })
-        loginreq.open('POST', `${API_ROOT}/login`, true)
-        loginreq.send(json_data)
+    const json_data = JSON.stringify({
+        username: username,
+        password: password,
     })
+    const x = await sendPromise('login', json_data)
+    switch (x.status) {
+        case 200:
+            console.assert(typeof x.response == 'boolean')
+            return x.response
+        default:
+            console.warn('Unknown status', x.status, x.response)
+            throw undefined
+    }
 }
 
-export function check_token(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-        let loginreq = new XMLHttpRequest()
-        loginreq.onload = ev => {
-            switch (loginreq.status) {
-                case 202:
-                    resolve(true)
-                    return
-                case 401:
-                    resolve(false)
-                    return
-                default:
-                    console.warn(
-                        'Unknown status',
-                        loginreq.status,
-                        loginreq.response
-                    )
-                    reject()
-            }
-        }
-        loginreq.onerror = ev => {
-            switch (loginreq.status) {
-                case 401:
-                    resolve(false)
-                    return
-                default:
-                    console.warn(
-                        'Unknown status',
-                        loginreq.status,
-                        loginreq.response
-                    )
-                    reject()
-            }
-        }
-        loginreq.onabort = ev => {
-            console.warn('Abort login', ev)
-            reject()
-        }
-        loginreq.open('GET', `${API_ROOT}/testtoken`, true)
-        loginreq.send()
-    })
+export async function check_token(): Promise<boolean> {
+    const x = await requestPromise('testtoken', { method: 'GET' })
+    switch (x.status) {
+        case 202:
+            return true
+        case 401:
+            return false
+        default:
+            console.warn('Unknown status', x.status, x.response)
+            throw undefined
+    }
 }
 
 export interface GroupSummary {
@@ -118,23 +83,14 @@ export interface GroupSummary {
     threadcount: number
 }
 
-export function summarize_groups(): Promise<GroupSummary[]> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = ev => {
-            if (postreq.status === 200) {
-                let groups = postreq.response as GroupSummary[]
-                resolve(groups)
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open('GET', `${API_ROOT}/summary`, true)
-        postreq.send()
-    })
+export async function summarize_groups(): Promise<GroupSummary[]> {
+    const x = await requestPromise('summary', { method: 'GET' })
+    if (x.status === 200) {
+        let groups = x.response as GroupSummary[]
+        return groups
+    } else {
+        throw undefined
+    }
 }
 
 interface ThreadSummaryRaw {
@@ -150,36 +106,30 @@ export interface ThreadSummary {
     languages: { lang: string; count: number }[]
 }
 
-export function summarize_threads(groupid: number): Promise<ThreadSummary[]> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = ev => {
-            if (postreq.status === 200) {
-                let groups = postreq.response as ThreadSummaryRaw[]
-                let parsed_groups = groups.map(v => {
-                    let languages = JSON.parse(v.languages) as {
-                        lang: string
-                        count: number
-                    }[]
-                    let new_threadsummary: ThreadSummary = {
-                        id: v.id,
-                        creator: v.creator,
-                        opened_on: new Date(v.opened_on),
-                        languages,
-                    }
-                    return new_threadsummary
-                })
-                resolve(parsed_groups)
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open('GET', `${API_ROOT}/summary_group/${groupid}`, true)
-        postreq.send()
+export async function summarize_threads(
+    groupid: number
+): Promise<ThreadSummary[]> {
+    const x = await requestPromise(`summary_group/${groupid}`, {
+        method: 'GET',
     })
+    if (x.status !== 200) {
+        throw undefined
+    }
+    let groups = x.response as ThreadSummaryRaw[]
+    let parsed_groups = groups.map(v => {
+        let languages = JSON.parse(v.languages) as {
+            lang: string
+            count: number
+        }[]
+        let new_threadsummary: ThreadSummary = {
+            id: v.id,
+            creator: v.creator,
+            opened_on: new Date(v.opened_on),
+            languages,
+        }
+        return new_threadsummary
+    })
+    return parsed_groups
 }
 
 export interface CorrectionSummary {
@@ -205,46 +155,39 @@ export interface PostSummary {
     corrections: CorrectionSummary[]
 }
 
-export function summarize_posts(thread_id: number): Promise<PostSummary[]> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = ev => {
-            if (postreq.status === 200) {
-                let posts = postreq.response as PostSummaryRaw[]
-                let parsed_posts = posts.map(v => {
-                    let raw_corrections = JSON.parse(v.corrections) as
-                        | CorrectionSummary[]
-                        | [null]
-                    let corrections: CorrectionSummary[]
-                    if (
-                        raw_corrections.length == 1 &&
-                        raw_corrections[0] === null
-                    ) {
-                        corrections = []
-                    } else {
-                        corrections = raw_corrections as CorrectionSummary[]
-                    }
-                    let new_postsummary: PostSummary = {
-                        id: v.id,
-                        opened_on: new Date(v.opened_on),
-                        ellipsis: v.ellipsis,
-                        username: v.username,
-                        lang: v.lang,
-                        corrections,
-                    }
-                    return new_postsummary
-                })
-                resolve(parsed_posts)
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open('GET', `${API_ROOT}/summary_thread/${thread_id}`, true)
-        postreq.send()
+export async function summarize_posts(
+    thread_id: number
+): Promise<PostSummary[]> {
+    const x = await requestPromise(`summary_thread/${thread_id}`, {
+        method: 'GET',
     })
+
+    if (x.status !== 200) {
+        throw undefined
+    }
+    let posts = x.response as PostSummaryRaw[]
+
+    let parsed_posts = posts.map(v => {
+        let raw_corrections = JSON.parse(v.corrections) as
+            | CorrectionSummary[]
+            | [null]
+        let corrections: CorrectionSummary[]
+        if (raw_corrections.length == 1 && raw_corrections[0] === null) {
+            corrections = []
+        } else {
+            corrections = raw_corrections as CorrectionSummary[]
+        }
+        let new_postsummary: PostSummary = {
+            id: v.id,
+            opened_on: new Date(v.opened_on),
+            ellipsis: v.ellipsis,
+            username: v.username,
+            lang: v.lang,
+            corrections,
+        }
+        return new_postsummary
+    })
+    return parsed_posts
 }
 
 export interface Post {
@@ -256,92 +199,58 @@ export interface Post {
     text: string
 }
 
-export function get_post(post_id: number): Promise<Post> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = ev => {
-            if (postreq.status === 200) {
-                let post = postreq.response as Post
-
-                post.posttime = new Date(
-                    Date.parse((post.posttime as unknown) as string)
-                )
-                resolve(post)
-            } else {
-                reject()
-            }
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open('GET', `${API_ROOT}/post/${post_id}`, true)
-        postreq.send()
+export async function get_post(post_id: number): Promise<Post> {
+    const x = await requestPromise(`post/${post_id}`, {
+        method: 'GET',
     })
+    if (x.status !== 200) {
+        throw undefined
+    }
+    let post = x.response as Post
+
+    post.posttime = new Date(Date.parse((post.posttime as unknown) as string))
+
+    return post
 }
 
-export function add_thread(
+export async function add_thread(
     group_id: number,
     post: string,
     langcode: string
 ): Promise<{ thread_id: number; post_id: number }> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            if (postreq.status !== 200) {
-                reject()
-                return
-            }
-            resolve(postreq.response)
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open(
-            'PUT',
-            `${API_ROOT}/addthread/${group_id}/${langcode}`,
-            true
-        )
-        postreq.send(post)
+    const x = await sendPromise(`addthread/${group_id}/${langcode}`, post, {
+        method: 'PUT',
     })
+    if (x.status !== 200) {
+        throw undefined
+    }
+    return x.response
 }
 
-export function add_post(
+export async function add_post(
     thread_id: number,
     post: string,
     langcode: string
 ): Promise<number> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            console.assert(typeof postreq.response == 'number')
-            resolve(postreq.response)
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open(
-            'PUT',
-            `${API_ROOT}/addpost/${thread_id}/${langcode}`,
-            true
-        )
-        postreq.send(post)
+    const x = await sendPromise(`addpost/${thread_id}/${langcode}`, post, {
+        method: 'PUT',
     })
+    console.assert(typeof x.response == 'number')
+    return x.response
 }
-export function add_correction(post: string, orig_id: number): Promise<number> {
-    return new Promise((resolve, reject) => {
-        let postreq = new XMLHttpRequest()
-        postreq.responseType = 'json'
-        postreq.onload = () => {
-            if (postreq.status !== 200) {
-                reject()
-                return
-            }
-            console.assert(typeof postreq.response == 'number')
-            resolve(postreq.response)
-        }
-        postreq.onerror = reject
-        postreq.onabort = reject
-        postreq.open('PUT', `${API_ROOT}/addcorrection/${orig_id}`, true)
-        postreq.send(post)
+
+export async function add_correction(
+    post: string,
+    orig_id: number
+): Promise<number> {
+    const x = await sendPromise(`addcorrection/${orig_id}`, post, {
+        method: 'PUT',
     })
+
+    if (x.status !== 200) {
+        throw undefined
+    }
+
+    console.assert(typeof x.response == 'number')
+    return x.response
 }
